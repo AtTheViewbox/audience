e37f8482a057e7e21e190214781a12c24c210da0
gangaramstyle <vineeth.gangaram@gmail.com>
2024-04-26 13:46:52 -0400

    webworker

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,14 @@ import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
   plugins: [react(), wasm(), topLevelAwait()],
+  worker: {
+    // Not needed with vite-plugin-top-level-await >= 1.3.0
+    // format: "es",
+    plugins: [
+      wasm(),
+      topLevelAwait()
+    ]
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
