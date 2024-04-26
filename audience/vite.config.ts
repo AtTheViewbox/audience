98a75b40032e6e0110098cce055bdda1a6dc11c2
gangaramstyle <vineeth.gangaram@gmail.com>
2024-04-26 10:47:14 -0400

    changed base

--- audience/vite.config.ts
+++ audience/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: "/audience/",
   plugins: [react()],
   resolve: {
     alias: {
