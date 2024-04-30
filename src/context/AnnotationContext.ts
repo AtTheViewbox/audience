030dab958cb5325ba56f123443de2ef17f2a7201
Ianto Lin Xi <iantolinxi@gmail.com>
2024-04-30 14:07:55 -0400

    more thinking around cursors and mouse movement

--- src/context/AnnotationContext.ts
+++ src/context/AnnotationContext.ts
@@ -7,17 +7,27 @@ import { getAnnotationManager } from '@cornerstonejs/tools/src/stateManagement/a
 export const attach = annotationManager => {
   eventTarget.addEventListener(
     TOOLS_EVENTS.ANNOTATION_ADDED,
-    () => {
-      console.log("ianto added", annotationManager);
+    (e) => {
+      console.log("ianto added", annotationManager, e);
     })
   eventTarget.addEventListener(
     TOOLS_EVENTS.ANNOTATION_MODIFIED,
-    () => {
-      console.log("ianto modified", annotationManager);
+    (e) => {
+      console.log("ianto modified", annotationManager, e);
     })
   eventTarget.addEventListener(
     TOOLS_EVENTS.ANNOTATION_REMOVED,
-    () => {
-      console.log("ianto removed", annotationManager);
+    (e) => {
+      console.log("ianto removed", annotationManager, e);
+    })
+
+  /* note that the below currently does not work,
+   * needs to actually be bound to an HTML element most likely.
+   * the question is... which one?
+   */
+  eventTarget.addEventListener(
+    TOOLS_EVENTS.MOUSE_MOVE,
+    (e) => {
+      console.log("ianto moved", annotationManager, e);
     })
 }
