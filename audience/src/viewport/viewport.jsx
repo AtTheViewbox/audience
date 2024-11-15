e94885389d2094d99e77edfc7dab42a7797680f5
Fei <mfei1225@gmail.com>
2024-11-14 17:52:23 -0700

    scrolling

--- audience/src/viewport/viewport.jsx
+++ audience/src/viewport/viewport.jsx
@@ -198,7 +198,7 @@ export default function Viewport(props) {
 
   return (
    <>
-      <div ref={elementRef} id={viewport_idx} style={{ width: '100%', height: '100%', overflow:"hidden"}} >
+      <div ref={elementRef} id={viewport_idx} style={{ width: '100%', height: '100%'}} >
         {isloading?<LoadingPage/>:null}
         </div>
     </>
